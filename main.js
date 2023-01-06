2f58caf877f474f4336edd39aac04917c54add56
Glavevy <sleepintheforst@gmail.com>
2023-01-06 11:07:16 +0200

    Make a 16x16 grid

---
+++ main.js
@@ -0,0 +1,16 @@
+function createDivs() {
+  for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+      row.classList.add("row");
+      main.appendChild(row);
+    for (let g = 0; g < 16; g++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+    }
+  }
+}
+
+const main = document.querySelector("#main");
+
+console.log(createDivs());
