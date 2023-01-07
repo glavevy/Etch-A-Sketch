d857132be7c7aec6a85dc7c846898dd40b0cdaf9
Glavevy <sleepintheforst@gmail.com>
2023-01-07 09:37:56 +0200

    Change grid size to user's input

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
+let compareNumber = 16;
+const main = document.querySelector("#main");
 function createDivs() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < compareNumber; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     main.appendChild(row);
-    for (let g = 0; g < 16; g++) {
+    for (let g = 0; g < compareNumber; g++) {
       const column = document.createElement("div");
       column.classList.add("column");
       row.appendChild(column);
       column.addEventListener("mouseenter", () => {
-        column.style.backgroundColor =  'blue';
+        column.style.backgroundColor = "#96DED1";
       });
     }
   }
 }
+createDivs();
 
-const main = document.querySelector("#main");
+const cells = document.querySelector("button");
 
-console.log(createDivs());
+cells.addEventListener("click", () => {
+  compareNumber = parseInt(prompt("Enter a Value", "0"));
+  if (compareNumber > 0 && compareNumber < 100) {
+    removeDivs();
+  } else {
+    alert("Wrong input!");
+  }
+});
+
+function removeDivs() {
+  while (main.firstChild) {
+    main.removeChild(main.lastChild);
+  }
+  createDivs();
+}
