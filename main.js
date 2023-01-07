1d29a6343901f3f74d3d8cf2458c284f02ef6b63
Glavevy <sleepintheforst@gmail.com>
2023-01-07 12:36:44 +0200

    Remove previous sketch when making a new one & make the grid always have the same size

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 let compareNumber = 16;
 const main = document.querySelector("#main");
+const cells = document.querySelector("button");
+
+createDivs();
+
 function createDivs() {
   for (let i = 0; i < compareNumber; i++) {
     const row = document.createElement("div");
+    let cellSize = (502 / compareNumber) - 2;
     row.classList.add("row");
     main.appendChild(row);
     for (let g = 0; g < compareNumber; g++) {
       const column = document.createElement("div");
       column.classList.add("column");
       row.appendChild(column);
+      column.style.width = cellSize + "px";
+      column.style.height = cellSize + "px";
+      console.log(column.style.width);
       column.addEventListener("mouseenter", () => {
         column.style.backgroundColor = "#96DED1";
       });
     }
   }
 }
-createDivs();
-
-const cells = document.querySelector("button");
 
 cells.addEventListener("click", () => {
   compareNumber = parseInt(prompt("Enter a Value", "0"));
